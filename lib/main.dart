9b15842960a495908d0ea22811ee69bc43043188
Mohax-Radwan <mohamed2004radwan@gmail.com>
2026-07-09 17:09:13 +0300

    structure the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nft_marketplace/app/app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,11 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
-    );
+    return const App();
   }
 }
